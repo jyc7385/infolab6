935298470b1318f3fca72cb539ba0ab88cd39df2
jyc7385 <jyc7385@naver.com>
2019-08-11 23:51:15 +0900

    190811 23:51 commit

--- R/kendallTest_sample.R
+++ R/kendallTest_sample.R
@@ -14,7 +14,7 @@ f_kendall <-function(x, k) {
     if(!require(Kendall)) install.packages("Kendall")
     library(Kendall)
 
-    ken<-Kendall::Kendall(x,k) ############### here is changed
+    ken<-Kendall(x,k) ############### here is changed
     p<-ken$sl
 
     return(p)
